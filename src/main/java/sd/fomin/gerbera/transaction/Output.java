051a8bd8b3da2cfa0af0067ae9150efa00739b6b
aafomin <foma.erkek@gmail.com>
2018-02-07 07:39:44 +0300

    Enhance transaction output validation

--- src/main/java/sd/fomin/gerbera/transaction/Output.java
+++ src/main/java/sd/fomin/gerbera/transaction/Output.java
@@ -11,6 +11,9 @@ import sd.fomin.gerbera.util.HexUtils;
 import java.util.Arrays;
 import java.util.List;
 
+import static sd.fomin.gerbera.util.ValidationUtils.isBase58;
+import static sd.fomin.gerbera.util.ValidationUtils.isEmpty;
+
 class Output {
 
     private final boolean mainNet;
@@ -19,9 +22,8 @@ class Output {
     private final OutputType type;
 
     Output(boolean mainNet, long satoshi, String destination, OutputType type) {
-        if (satoshi <= 0) {
-            throw new IllegalArgumentException("Amount of satoshi must be a positive value");
-        }
+        validateOutputData(mainNet, satoshi, destination);
+
         this.mainNet = mainNet;
         this.satoshi = satoshi;
         this.destination = destination;
@@ -50,8 +52,6 @@ class Output {
     }
 
     private byte[] getLockingScript(boolean mainNet, String destination) {
-        validateDestinationAddress(mainNet, destination);
-
         byte[] decodedAddress = Base58CheckUtils.decode(destination);
         byte[] hash = Arrays.copyOfRange(decodedAddress, 1, decodedAddress.length);
 
@@ -78,21 +78,6 @@ class Output {
         return lockingScript.bytes();
     }
 
-    private void validateDestinationAddress(boolean mainNet, String destination) {
-        if (destination == null || destination.isEmpty()) {
-            throw new IllegalArgumentException("Destination address must not be empty");
-        }
-
-        List<Character> prefixP2PKH = mainNet ? Arrays.asList('1') : Arrays.asList('m', 'n');
-        List<Character> prefixP2SH = Arrays.asList(mainNet ? '3' : '2');
-        char prefix = destination.charAt(0);
-
-        if (!prefixP2PKH.contains(prefix) && !prefixP2SH.contains(prefix)) {
-            throw new IllegalArgumentException("Only destination addresses starting with " + prefixP2PKH + " (P2PKH) " +
-                    "or " + prefixP2SH + " (P2SH) supported.");
-        }
-    }
-
     long getSatoshi() {
         return satoshi;
     }
@@ -100,5 +85,35 @@ class Output {
     @Override
     public String toString() {
         return new StringBuilder().append(destination).append(" ").append(satoshi).toString();
+    }
+
+    private void validateOutputData(boolean mainNet, long satoshi, String destination) {
+        validateDestinationAddress(mainNet, destination);
+        validateAmount(satoshi);
+    }
+
+    private void validateAmount(long satoshi) {
+        if (satoshi <= 0) {
+            throw new IllegalArgumentException("Amount of satoshi must be a positive value");
+        }
+    }
+
+    private void validateDestinationAddress(boolean mainNet, String destination) {
+        if (isEmpty(destination)) {
+            throw new IllegalArgumentException("Address must not be null or empty");
+        }
+
+        if (!isBase58(destination)) {
+            throw new IllegalArgumentException("Address must contain only base58 characters");
+        }
+
+        List<Character> prefixP2PKH = mainNet ? Arrays.asList('1') : Arrays.asList('m', 'n');
+        List<Character> prefixP2SH = Arrays.asList(mainNet ? '3' : '2');
+        char prefix = destination.charAt(0);
+
+        if (!prefixP2PKH.contains(prefix) && !prefixP2SH.contains(prefix)) {
+            throw new IllegalArgumentException("Only addresses starting with " + prefixP2PKH + " (P2PKH) " +
+                    "or " + prefixP2SH + " (P2SH) supported.");
+        }
     }
 }
--- src/test/java/sd/fomin/gerbera/transaction/TransactionOutputValidationTest.java
+++ src/test/java/sd/fomin/gerbera/transaction/TransactionOutputValidationTest.java
@@ -5,6 +5,36 @@ import org.junit.Test;
 public class TransactionOutputValidationTest {
 
     @Test(expected = IllegalArgumentException.class)
+    public void testNullDestination() {
+        TransactionBuilder.create().to(null, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyDestination() {
+        TransactionBuilder.create().to("   ", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNotBase58Destination() {
+        TransactionBuilder.create().to("1NZUP3JAc9JkmbvlmoTv7nVgZGtyJjirKV1", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIncorrectDestinationPrefixMainNet() {
+        TransactionBuilder.create().to("2NZUP3JAc9JkmbvmoTv7nVgZGtyJjirKV1", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIncorrectDestinationPrefixTestNet() {
+        TransactionBuilder.create(false).to("1NZUP3JAc9JkmbvmoTv7nVgZGtyJjirKV1", 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroSatochi() {
+        TransactionBuilder.create().to("1NZUP3JAc9JkmbvmoTv7nVgZGtyJjirKV1", 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testNegativeSatochi() {
         TransactionBuilder.create().to("1NZUP3JAc9JkmbvmoTv7nVgZGtyJjirKV1", -1);
     }
