b2fa776dc7ee035ac9afb600c7888f36306c2760
vladmelnyk <v.v.melnyk1994@gmail.com>
2020-12-29 22:04:26 +0200

    Fix ltc decode segwit (#8)
    * create locking scripts for bech32 properly

--- src/main/java/sd/fomin/gerbera/transaction/ScriptPubKeyProducer.java
+++ src/main/java/sd/fomin/gerbera/transaction/ScriptPubKeyProducer.java
@@ -1,5 +1,6 @@
 package sd.fomin.gerbera.transaction;
 
+import sd.fomin.gerbera.constant.ErrorMessages;
 import sd.fomin.gerbera.constant.OpCodes;
 import sd.fomin.gerbera.types.Coin;
 import sd.fomin.gerbera.types.OpSize;
@@ -14,7 +15,7 @@ public interface ScriptPubKeyProducer {
 
     byte[] produceScript(byte[] hash);
 
-    static ScriptPubKeyProducer getInstance(boolean mainNet, Coin coin, byte prefix) {
+    static ScriptPubKeyProducer getInstance(boolean mainNet, Coin coin, byte prefix, boolean isBech32) {
         List<Byte> prefixesP2PKHMainnet;
         List<Byte> prefixesP2PKHTestnet;
         List<Byte> prefixesP2SHMainnet;
@@ -28,7 +29,7 @@ public interface ScriptPubKeyProducer {
                 prefixesP2SHTestnet = singletonList((byte) 0xC4);
                 break;
             case LTC:
-                prefixesP2PKHMainnet = singletonList((byte) 0x30);
+                prefixesP2PKHMainnet = asList((byte) 0x30, (byte) 0x00);
                 prefixesP2PKHTestnet = singletonList((byte) 0x6F);
                 prefixesP2SHMainnet = asList((byte) 0x05, (byte) 0x32);
                 prefixesP2SHTestnet = singletonList((byte) 0xC4);
@@ -36,7 +37,17 @@ public interface ScriptPubKeyProducer {
             default:
                 throw new IllegalStateException("Unexpected value: " + coin);
         }
-        if ((mainNet ? prefixesP2PKHMainnet : prefixesP2PKHTestnet).contains(prefix)) {
+        if (isBech32) {
+            //P2WSH & P2WKH
+            return hash -> {
+                ByteBuffer lockingScript = new ByteBuffer();
+                lockingScript.append(OpCodes.FALSE);
+                lockingScript.append(OpSize.ofInt(hash.length).getSize());
+                lockingScript.append(hash);
+
+                return lockingScript.bytes();
+            };
+        } else if ((mainNet ? prefixesP2PKHMainnet : prefixesP2PKHTestnet).contains(prefix)) {
             //P2PKH
             return hash -> {
                 ByteBuffer lockingScript = new ByteBuffer();
@@ -58,18 +69,9 @@ public interface ScriptPubKeyProducer {
             };
             //TODO Provide appropriate condition to verify P2WSH
         } else {
-            //P2WSH & P2WKH
-            return hash -> {
-                ByteBuffer lockingScript = new ByteBuffer();
-                lockingScript.append(OpCodes.FALSE);
-                lockingScript.append(OpSize.ofInt(hash.length).getSize());
-                lockingScript.append(hash);
-
-                return lockingScript.bytes();
-            };
+            throw new IllegalArgumentException(String.format(ErrorMessages.SPK_UNSUPPORTED_PRODUCER, mainNet, prefix));
         }
 
-//        throw new IllegalArgumentException(String.format(ErrorMessages.SPK_UNSUPPORTED_PRODUCER, mainNet, prefix));
     }
 
 }
--- src/test/java/sd/fomin/gerbera/transaction/ScriptPubKeyProducerTest.java
+++ src/test/java/sd/fomin/gerbera/transaction/ScriptPubKeyProducerTest.java
@@ -10,7 +10,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2PKHMainNet() {
-        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.BTC, (byte) 0x00)
+        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.BTC, (byte) 0x00, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "76" + "A9" + "05" + "0001020304" + "88" + "AC";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -18,7 +18,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2PKHTestNet() {
-        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.BTC, (byte) 0x6F)
+        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.BTC, (byte) 0x6F, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "76" + "A9" + "05" + "0001020304" + "88" + "AC";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -26,7 +26,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2SHMainNet() {
-        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.BTC, (byte) 0x05)
+        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.BTC, (byte) 0x05, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "A9" + "05" + "0001020304" + "87";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -34,7 +34,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2SHTestNet() {
-        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.BTC, (byte) 0xC4)
+        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.BTC, (byte) 0xC4, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "A9" + "05" + "0001020304" + "87";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -42,7 +42,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2PKHMainNetLtc() {
-        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x30)
+        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x30, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "76" + "A9" + "05" + "0001020304" + "88" + "AC";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -50,7 +50,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2PKHTestNetLtc() {
-        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.LTC, (byte) 0x6F)
+        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.LTC, (byte) 0x6F, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "76" + "A9" + "05" + "0001020304" + "88" + "AC";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -58,7 +58,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2SHMainNetLtc() {
-        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x05)
+        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x05, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "A9" + "05" + "0001020304" + "87";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -66,7 +66,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2SHMainNetLtcNewFormat() {
-        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x32)
+        byte[] script = ScriptPubKeyProducer.getInstance(true, Coin.LTC, (byte) 0x32, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "A9" + "05" + "0001020304" + "87";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
@@ -74,7 +74,7 @@ public class ScriptPubKeyProducerTest {
 
     @Test
     public void testP2SHTestNetLtc() {
-        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.LTC, (byte) 0xC4)
+        byte[] script = ScriptPubKeyProducer.getInstance(false, Coin.LTC, (byte) 0xC4, false)
                 .produceScript(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04});
         String expectedScript = "A9" + "05" + "0001020304" + "87";
         assertThat(script).isEqualTo(HexUtils.asBytes(expectedScript));
