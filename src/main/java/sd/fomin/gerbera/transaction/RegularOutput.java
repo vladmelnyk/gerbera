65ebabf3ddc219aa813e28295e092862bf9de13b
Vladyslav Melnyk <v.v.melnyk1994@gmail.com>
2020-12-29 21:44:31 +0200

    create locking scripts for bech32 properly

--- src/main/java/sd/fomin/gerbera/transaction/RegularOutput.java
+++ src/main/java/sd/fomin/gerbera/transaction/RegularOutput.java
@@ -35,8 +35,8 @@ class RegularOutput extends Output {
     @Override
     protected byte[] getLockingScript() {
         return isBech32
-                ? ScriptPubKeyProducer.getInstance(mainNet, coin, decodedAddress[0]).produceScript(decodedAddress)
-                : ScriptPubKeyProducer.getInstance(mainNet, coin, decodedAddress[0])
+                ? ScriptPubKeyProducer.getInstance(mainNet, coin, decodedAddress[0], true).produceScript(decodedAddress)
+                : ScriptPubKeyProducer.getInstance(mainNet, coin, decodedAddress[0], false)
                 .produceScript(Arrays.copyOfRange(decodedAddress, 1, decodedAddress.length));
     }
 
